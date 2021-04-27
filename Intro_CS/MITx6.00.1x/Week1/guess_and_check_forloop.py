51e214727d57d28b1e8354adc82ef0cba7996d1f
Iván E. Hernández T <iamieht@gmail.com>
2021-04-27 13:58:53 +0200

    Update guess_and_check_forloop.py

--- Intro_CS/MITx6.00.1x/Week1/guess_and_check_forloop.py
+++ Intro_CS/MITx6.00.1x/Week1/guess_and_check_forloop.py
@@ -1,4 +1,10 @@
 x = int(input('Enter an integer: '))
-for guess in range(x+1):
-    if guess**3 == x:
-        print('Cube root of ', x, ' is ', guess)
+for guess in range(abs(x)+1):
+    if guess**3 >= abs(x):
+        break
+if guess**3 != abs(x):
+    print(x, 'is not a perfect cube')
+else:
+    if x < 0:
+        guess = - guess
+    print('Cube root of ', x, ' is ', guess)
