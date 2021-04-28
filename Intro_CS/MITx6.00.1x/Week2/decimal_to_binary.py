02b704f6cd0600c420afcdf3f5dee43e4606bc83
Ivan E. Hernandez T <iamieht@gmail.com>
2021-04-28 17:53:03 +0200

    Create decimal_to_binary.py

---
+++ Intro_CS/MITx6.00.1x/Week2/decimal_to_binary.py
@@ -0,0 +1,18 @@
+num = int(input('WHat is the number you want to convert to binary? '))
+
+if num < 0:
+    isNeg = True
+    num = abs(num)
+else:
+    isNeg = False
+
+result = ''
+if num == 0:
+    result = '0'
+while num > 0:
+    result = str(num%2) + result
+    num = num//2
+if isNeg:
+    result = '-' + result
+
+print(result)
