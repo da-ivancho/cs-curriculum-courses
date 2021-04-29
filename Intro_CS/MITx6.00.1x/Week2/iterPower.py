8dcf218e5f6568272e30ddc1ed454096c55d41b8
Iván E. Hernández T <iamieht@gmail.com>
2021-04-29 12:03:11 +0200

    Create iterPower.py

---
+++ Intro_CS/MITx6.00.1x/Week2/iterPower.py
@@ -0,0 +1,16 @@
+def iterPower(base, exp):
+    '''
+    base: float or int
+    exp: int >= 0
+    return int or float
+    '''
+    result = 1
+    while exp > 0:
+        result *= base
+        exp -= 1
+    
+    return result
+
+print(iterPower(-3.77, 0))
+print(iterPower(-4.97, 6))
+print(iterPower(9.78, 7))
