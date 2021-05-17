4314e1dac9113f6101eaf84a13fdd0a4a8682f14
Iván E. Hernández T <iamieht@gmail.com>
2021-05-17 18:34:30 +0200

    Update coordinate.py

--- Intro_CS/MITx6.00.1x/Week5/coordinate.py
+++ Intro_CS/MITx6.00.1x/Week5/coordinate.py
@@ -6,6 +6,15 @@ class Coordinate(object):
         self.x = x
         self.y = y
     
+    def getX(self):
+        # Getter method for a Coordinate object's x coordinate.
+        # Getter methods are better practice than just accessing an attribute directly
+        return self.x
+    
+    def getY(self):
+        # Getter method for a Coordinate object's y coordinate
+        return self.y
+    
     def distance(self, other):
         x_diff_sq = (self.x - other.x) **2
         y_diff_sq = (self.y - other.y) **2
@@ -16,6 +25,12 @@ class Coordinate(object):
     
     def __sub__(self, other):
         return Coordinate(self.x - other.x, self.y - other.y)
+    
+    def __eq__(self, other):
+        return self.getX() == other.getX() and self.getY() == other.getY()
+    
+    def __repr__(self):
+        return 'Coordinate(' + str(self.getX()) + ',' + str(self.getY()) + ')'
 
 
 #Tests
